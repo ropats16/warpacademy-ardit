d5e45c8b5c23bfb626ec45de9bd1d43d9e0a7b17
Ropats <rohitcpathare@gmail.com>
2023-01-10 19:27:22 +0530

    Add function for reading messages

---
+++ src/contracts/actions/read/readMessage.ts
@@ -0,0 +1,18 @@
+// src/contracts/actions/read/readMessage.ts
+
+import { ArditAction, ArditState, ContractResult } from "../../types/types";
+
+declare const ContractError;
+
+export const readMessage = async (
+  state: ArditState,
+  { input: { id } }: ArditAction
+): Promise<ContractResult> => {
+  const message = state.messages.find((m) => m.id == id);
+
+  if (!message) {
+    throw new ContractError(`Message with id: ${id} does not exist`);
+  }
+
+  return { result: message };
+};
